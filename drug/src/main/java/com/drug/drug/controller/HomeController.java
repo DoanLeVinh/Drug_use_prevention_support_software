ce6d09d4e5dfdf113c03cc0f5f08dfefe266b3e8
DoanLeVinh <vinhdl1636@ut.edu.vn>
2025-06-25 06:09:11 +0700

    connect staff

--- drug/src/main/java/com/drug/drug/controller/HomeController.java
+++ drug/src/main/java/com/drug/drug/controller/HomeController.java
@@ -3,7 +3,6 @@ package com.drug.drug.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class HomeController {
     @GetMapping({"/", "/index"})
@@ -13,7 +12,7 @@ public class HomeController {
 
     @GetMapping("/login")
     public String login() {
-        return "Login"; // Đúng tên file login.html (chữ thường)
+        return "login"; // Đã sửa thành chữ thường
     }
 
     @GetMapping("/register")
@@ -30,21 +29,43 @@ public class HomeController {
     public String courses() {
         return "member/courses";
     }
+
     @GetMapping("/assessment")
     public String assessment() {
         return "member/assessment";
     }
+
     @GetMapping("/consultation")
     public String consultation() {
         return "member/consultation";
     }
+
     @GetMapping("/blog")
     public String blog() {
         return "member/blog";
     }
+
     @GetMapping("/about")
     public String about() {
         return "member/about";
     }
-    
+
+    @GetMapping("/dashboard") 
+    public String dashboard() {
+        return "staff/dashboard";
+    }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "staff/layout";
+    }
+
+    @GetMapping("/user-management")
+    public String userManagement() {
+        return "staff/user-management";
+    }
+    @GetMapping("/appointments")
+    public String appointments() {
+        return "staff/appointments";
+    }
 }
